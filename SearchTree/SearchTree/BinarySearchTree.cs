5f6c5794c6aceb8e86c561a648cf2d6cb9df94bc
dana <danaw09@gmail.com>
2019-07-16 20:30:00 -0500

    ctor search and add method to the binary search tree

--- SearchTree/SearchTree/BinarySearchTree.cs
+++ SearchTree/SearchTree/BinarySearchTree.cs
@@ -8,11 +8,46 @@ namespace SearchTree
 {
     class BinarySearchTree
     {
-       
-
-        
-
-
-
-    }
-}
+        Node root;
+        int count = 0;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void AddNode(int number)
+        {
+            if (root == null)
+            {
+                root = new Node(number);
+            }
+            else
+            {
+                root.InsertNode(ref root, number);
+            }
+            count++;
+        }
+        public void Display()
+        {
+            root.ReceiveNumber(root);
+        }
+        public bool search(Node node, int data)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.value == data)
+            {
+                return true;
+            }
+            else if (node.value < data)
+            {
+                return search(node.left, data);
+            }
+            else if (node.value > data)
+            {
+                return search(node.right, data);
+            }
+            return false;
+        }
+    }   }   
